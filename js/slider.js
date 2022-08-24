162ed2d4b878429da5ec959d28be33f38a1ebc42
FabioLongoni <longoni.fabio90@gmail.com>
2022-08-24 15:31:16 +0200

    img-check-data-Vue

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,21 @@
 // console.log('slider')
 
-const slides = [
-	'./img/01.jpg',
-	'./img/02.jpg',
-	'./img/03.jpg',
-	'./img/04.jpg',
-	'./img/05.jpg',
-]
+// const slides = [
+// 	'./img/01.jpg',
+// 	'./img/02.jpg',
+// 	'./img/03.jpg',
+// 	'./img/04.jpg',
+// 	'./img/05.jpg',
+// ]
 
 const app = new Vue ({
 	el: '#root',
 	data: {
-		slides,
+		img1: './img/01.jpg',
+		img2: './img/02.jpg',
+		img3: './img/03.jpg',
+		img4: './img/04.jpg',
+		img5: './img/05.jpg',
 	},	
 })
 
